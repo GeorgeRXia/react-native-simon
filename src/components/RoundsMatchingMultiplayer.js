0921821a3e109d5b22f9cea6ce334b2f49a1d997
George Xia <georxia@gmail.com>
2017-06-26 16:36:18 -0400

    working multiplayer rounds

--- src/components/RoundsMatchingMultiplayer.js
+++ src/components/RoundsMatchingMultiplayer.js
@@ -34,17 +34,18 @@ class RoundsMatchingMultiplayer extends Component {
       startGame: props.startGame,
       opponentsGameStatus: "",
       ownGameStatus: "",
-      GameSetter: "",
+      gameSetter: "",
       opponentInGame: "",
       turn: "",
       points: 0,
+      opponentsPoints:0,
       speed: 1
 
 
     };
 
   }
-  startWs() {
+  startWs(){
 
     this.ws = new WebSocket('ws://localhost:3001');
 
@@ -56,19 +57,19 @@ class RoundsMatchingMultiplayer extends Component {
 
 
   }
-  setTimeOut() {
-    var that = this;
+  setTimeOut(){
+  var that = this;
 
     this.intervalPlayer = setInterval(() => {
 
       this.otherPlayer();
-      console.log("This is looping");
+        console.log("This is looping");
 
-      if (that.state.opponentInGame === true) {
-        that.startWs();
-        clearInterval(that.intervalPlayer);
+        if(that.state.opponentInGame === true){
+          that.startWs();
+          clearInterval(that.intervalPlayer);
 
-      }
+        }
 
 
     }, 1000)
@@ -76,93 +77,82 @@ class RoundsMatchingMultiplayer extends Component {
 
   }
 
-  componentWillMount() {
+    componentWillMount(){
 
-    var that = this;
-    axios.post('http://localhost:3000/games', {
-      data: {
-        user_id: 2
+      var that = this;
+      axios.post('http://localhost:3000/games',{
+        data:{
+            user_id: 2
 
-      }
-    }).then(function(response) {
-      console.log(response.data);
-      that.setState({
-        opponentInGame: response.data,
-        turn: "original"
-      }, function() {;
-        if (response.data) {
-          that.addToSequence();
         }
+      }).then(function(response){
+        console.log(response.data);
+        that.setState({opponentInGame: response.data, turn: "original"},function(){;
+        if (response.data){
+        that.addToSequence();
+      }
+    })
+    })
+
+  this.setTimeOut()
+
+
+    }
+
+    otherPlayer(){
+      var that = this;
+
+      axios.get('http://localhost:3000/games').then(function(response){
+
+        that.setState({opponentInGame: response.data});
       })
-    })
+    }
 
-    this.setTimeOut()
-
-
-  }
-
-  otherPlayer() {
-    var that = this;
-    console.log("hey");
-    axios.get('http://localhost:3000/games').then(function(response) {
-      console.log(response.data);
-      that.setState({
-        opponentInGame: response.data
-      });
-    })
-  }
-
-  onOpenConnection = () => {
+  onOpenConnection = () =>{
     console.log("open");
 
 
   }
 
-  onScoreReceived = (event) => {
+  onScoreReceived = (event) =>{
 
-    console.log(event.data);
+  console.log(event.data);
 
-    let receivedEvent = JSON.parse(event.data)
-    var that = this;
+   let receivedEvent = JSON.parse(event.data)
+   var that = this;
 
-    if (receivedEvent.gameStatus === "done") {
-      this.setState({
-        opponentsGameStatus: receivedEvent.gameStatus,
-        computerSequence: receivedEvent.computerSequence
-      }, function() {
-        that.animateSequence(receivedEvent.computerSequence)
-      })
+  if(receivedEvent.gameStatus === "done"){
+    this.setState({opponentsGameStatus: receivedEvent.gameStatus,
+      computerSequence: receivedEvent.computerSequence
+    },function(){
+      that.animateSequence(receivedEvent.computerSequence)})
 
 
 
-    } else if (receivedEvent.turn === "other") {
+  }else if(receivedEvent.turn === "other"){
 
-      this.setState({
-        computerSequence: receivedEvent.computerSequence,
-        turn: receivedEvent.turn
-      }, function() {
-        that.animateSequence(receivedEvent.computerSequence);
+    this.setState({computerSequence: receivedEvent.computerSequence, turn: receivedEvent.turn, opponentsPoints: receivedEvent.points},function(){
+      that.animateSequence(receivedEvent.computerSequence);
 
-      })
+    })
 
 
 
-    } else if (receivedEvent.turn === "original") {
-      this.addToRound();
+  }else if(receivedEvent.turn === "original"){
+    this.setState({opponentsPoints: receivedEvent.points});
+    this.addToRound();
 
-    } else if (receivedEvent.gameStatus === "loser" || receivedEvent.gameStatus === "winner" || receivedEvent.gameStatus === "tie") {
+  } else if(receivedEvent.gameStatus === "loser" || receivedEvent.gameStatus === "winner" ||receivedEvent.gameStatus === "tie" ){
 
-      this.setState({
-        gameSetter: receivedEvent.gameStatus
-      })
+    this.setState({gameSetter: receivedEvent.gameStatus})
 
-    } else {
-      this.setState({
-        history: event.data
+  }else{
+    this.setState({
+      history: event.data
 
-      })
+    })
 
-    }
+  }
 
 
   }
@@ -173,7 +163,7 @@ class RoundsMatchingMultiplayer extends Component {
 
   onClose = (event) => {
 
-    console.log("onclose", event.code, event.reason);
+  console.log("onclose", event.code, event.reason);
 
   }
 
@@ -181,52 +171,36 @@ class RoundsMatchingMultiplayer extends Component {
     console.log(status);
     let computerSequence = this.state.computerSequence;
 
-    if (status === "done") {
+    if(status === "done"){
       this.setState({
-        ownGameStatus: "done"
-      })
-      let game = {
-        gameStatus: "done",
-        computerSequence: computerSequence
-      }
+      ownGameStatus: "done" })
+      let game = {gameStatus: "done", computerSequence: computerSequence}
 
       this.ws.send(JSON.stringify(game));
-    } else if (status === "original") {
+    }else if(status === "original"){
 
-      let game = {
-        turn: "original",
-        computerSequence: computerSequence
-      }
+    let game = {turn: "original", computerSequence: computerSequence, points: this.state.points};
+    this.ws.send(JSON.stringify(game));
+  }else if (status === "loser"){
+      let game = {gameStatus: "loser"}
       this.ws.send(JSON.stringify(game));
-    } else if (status === "loser") {
-      let game = {
-        gameStatus: "loser"
-      }
-      this.ws.send(JSON.stringify(game));
-    } else if (status === "winner") {
-      console.log("it got into the winner part");
-      let game = {
-        gameStatus: "winner"
-      }
-      this.ws.send(JSON.stringify(game));
+  }else if (status === "winner"){
+    console.log("it got into the winner part");
+    let game = {gameStatus: "winner"}
+    this.ws.send(JSON.stringify(game));
 
-    } else if (status === "tieGame") {
+  }else if (status === "tieGame") {
 
-      let game = {
-        gameStatus: "tie"
-      }
-      this.ws.send(JSON.stringify(game));
+    let game = {gameStatus: "tie"}
+  this.ws.send(JSON.stringify(game));
 
-    } else {
+  }else{
 
-      let game = {
-        turn: "other",
-        computerSequence: computerSequence
-      }
-      this.ws.send(JSON.stringify(game));
+    let game = {turn: "other", computerSequence: computerSequence, points: this.state.points};
+  this.ws.send(JSON.stringify(game));
 
 
-    }
+  }
 
   }
 
@@ -234,97 +208,62 @@ class RoundsMatchingMultiplayer extends Component {
 
 
   render() {
-    if (this.state.gameSetter === "winner" || this.state.gameSetter === "loser" || this.state.gameSetter === "tie") {
+  if(this.state.gameSetter === "winner" || this.state.gameSetter === "loser" || this.state.gameSetter === "tie"){
 
-      return ( <
-        View >
+    return (
+      <View>
 
-        <
-        Text style = {
-          styles.score
-        } > {
-          this.state.round
-        } < /Text>
+      {this.gameStatus(this.state.gameSetter)}
 
-        <
-        View style = {
-          styles.container
-        } >
+      <View style={styles.container} >
 
-        {
-          this.renderTiles()
-        }
+      {this.renderTiles()}
 
-        <
-        /View> {
-          this.gameStatus(this.state.gameSetter)
-        } <
-        /View>
-      )
+      </View>
+
+      </View>
+    )
 
 
-    } else if (this.state.ownGameStatus === "done") {
-      return ( <
-        View >
+  }else if (this.state.ownGameStatus === "done"){
+    return (
+      <View>
 
-        <
-        Text style = {
-          styles.score
-        } > {
-          this.state.round
-        } < /Text>
+      <Text style={styles.score}> {this.state.round} </Text>
+        <Text style={styles.score}> {this.state.points} </Text>
+          <Text style={styles.score}> {this.state.opponentsPoints} </Text>
+      <View style={styles.container} >
 
-        <
-        View style = {
-          styles.container
-        } >
+      {this.renderTiles()}
 
-        {
-          this.renderTiles()
-        }
+      </View>
 
-        <
-        /View>
+      </View>
+    )
 
-        <
-        /View>
-      )
+  }else{
+        return (
+          <View>
+          <Text style={styles.score}>{this.state.round} </Text>
+          <Text style={styles.score}> {this.state.points} </Text>
+            <Text style={styles.score}> {this.state.opponentsPoints} </Text>
+          <View style={styles.container} >
 
-    } else {
-      return ( <
-        View >
-        <
-        Text style = {
-          styles.score
-        } > {
-          this.state.round
-        } < /Text>
+          {this.renderTiles()}
 
+          </View>
 
-        <
-        View style = {
-          styles.container
-        } >
+          </View>
+        )
 
-        {
-          this.renderTiles()
-        }
-
-        <
-        /View>
-
-        <
-        /View>
-      )
-
-    }
+      }
 
   }
 
   renderTiles() {
     let result = [];
     let i = 1;
-    let bgColors = ["", "", "#3275DD", "#D93333", "#64D23B", "#FED731"];
+    let bgColors = ["", "","#3275DD", "#D93333", "#64D23B", "#FED731"];
     for (var row = 0; row < SIZE; row++) {
       for (var col = 0; col < SIZE; col++) {
         var position = {
@@ -332,11 +271,7 @@ class RoundsMatchingMultiplayer extends Component {
           top: row * CELL_SIZE + CELL_PADDING
         };
 
-        result.push(this.renderTile(i++, position, {
-          backgroundColor: bgColors[i]
-        }, {
-          backgroundColor: 'black'
-        }));
+        result.push(this.renderTile(i++, position, {backgroundColor: bgColors[i]}, {backgroundColor: 'black'}));
 
       }
     }
@@ -346,183 +281,226 @@ class RoundsMatchingMultiplayer extends Component {
 
   renderTile(id, position, bgColor, litBgColor) {
 
-    return ( < TouchableOpacity onPress = {
-        () => this.playTheGame(id)
-      } >
-      <
-      View style = {
-        [styles.tile, position, this.state.lit == id ? litBgColor : bgColor]
-      } > < Text style = {
-        styles.letter
-      } > {
-        id
-      } < /Text> <
-      /View> <
-      /TouchableOpacity>)
-    }
-    playTheGame(id) {
+    return (<TouchableOpacity onPress={() => this.playTheGame(id)}>
+      <View style={[styles.tile, position, this.state.lit == id ? litBgColor : bgColor]}><Text style={styles.letter}>{id}</Text>
+      </View>
+    </TouchableOpacity>)
+  }
+  playTheGame(id){
 
-      let computerSequence = this.state.computerSequence;
-      let playerSequence = this.state.playerSequence;
-      playerSequence.push(id)
-      let i = playerSequence.length - 1;
+    let computerSequence = this.state.computerSequence;
+    let playerSequence = this.state.playerSequence;
+    playerSequence.push(id)
+    let i = playerSequence.length - 1;
 
 
-      if (computerSequence[i] === playerSequence[i]) {
-        console.log(playerSequence);
-        this.setState({
-          playerSequence: playerSequence
-        })
-        if (computerSequence.length === playerSequence.length) {
+    if(computerSequence[i] === playerSequence[i] ){
+    console.log(playerSequence);
+      this.setState({playerSequence: playerSequence})
+      if (computerSequence.length === playerSequence.length){
 
-          if (this.state.opponentsGameStatus === "done") {
-            this.setState({
-              gameSetter: "winner"
+         if (this.state.turn === "other"){
+           console.log("it is in the other");
+          let currentRound = this.state.round;
+
+          let points = 100 * currentRound + this.state.points;
+          currentRound++;
+          var that = this;
+          this.setState({round: currentRound, playerSequence: [], points: points},function(){
+            console.log(that.state.points);
+              console.log(that.state.opponentsPoints);
+              let status = "";
+              let ownStatus = "";
+            if(that.state.round === 4){
+                if(that.state.points > this.state.opponentsPoints){
+                  status = "loser";
+                  ownStatus = "winner";
+                }else if(that.state.points < this.state.opponentsPoints){
+                  status  = "winner";
+                  ownStatus = "loser";
+                }else{
+                  status = "tieGame";
+                  ownStatus = "tie"
+                }
+                this.setState({gameSetter: ownStatus},function(){
+                    that.onSendScore(status)
+
+                })
+
+
+            }else{
+              that.onSendScore("original");
+
+            }
+
+          });
+
+
+          }else{
+            console.log("it will now update score");
+            let points = 100 * this.state.round + this.state.points;
+              console.log(points);
+            this.setState({points: points},function(){
+                this.onSendScore();
             })
-            this.onSendScore("loser");
 
-
-          } else if (this.state.turn === "other") {
-            let currentRound = this.state.round;
-            currentRound++;
-
-
-            this.setState({
-              round: currentRound,
-              playerSequence: []
-            });
-            this.onSendScore("original");
-
-          } else {
-
-            this.onSendScore();
 
           }
-        }
-
-      } else {
-        console.log("Gameover");
-        if (this.state.opponentsGameStatus === "done") {
-          console.log("Gameover2");
-          this.tieGame();
-        } else if (this.state.turn != "original") {
-          this.setState({
-            gameSetter: "loser"
-          })
-          this.onSendScore("winner");
-
-
-        } else {
-          this.turnOver();
-        }
       }
 
-    }
-
-    addToRound() {
-
+   }else {
+     var that = this;
+      if (this.state.turn === "other"){
+      let currentPoints = this.state.points;
       let currentRound = this.state.round;
-      currentRound++;
+      currentRound ++;
+      let points = currentPoints - 100;
       var that = this;
-      this.setState({
-        round: currentRound,
-        playerSequence: []
+      this.setState({round: currentRound, playerSequence: [], points: points},function(){
+        if(that.state.round === 4){
+
+          if(that.state.points > this.state.opponentsPoints){
+            status = "winner";
+            ownStatus = "loser";
+          }else if(that.state.points < this.state.opponentsPoints){
+            status  = "loser";
+            ownStatus = "winner";
+          }else{
+            status = "tieGame";
+            ownStatus = "tie"
+          }
+          this.setState({gameSetter: ownStatus})
+          that.onSendScore(status)
+
+        }else{
+
+          that.onSendScore("original");
+        }
       });
 
 
-      this.addToSequence();
-    }
+      }else{
+        console.log("it will now update score");
+        let currentPoints = this.state.points;
+        let points = currentPoints - 100;
+        this.setState({points: points},function(){
+          if(that.state.round === 4){
 
-    addToSequence() {
+            if(that.state.points > this.state.opponentsPoints){
+              status = "winner";
+              ownStatus = "loser";
+            }else if(that.state.points < this.state.opponentsPoints){
+              status  = "loser";
+              ownStatus = "winner";
+            }else{
+              status = "tieGame";
+              ownStatus = "tie"
+            }
+            this.setState({gameSetter: ownStatus})
+            that.onSendScore(status)
 
-      let addToSequence = Math.floor((Math.random() * 4) + 1)
-      let newSequence = this.state.computerSequence
-      newSequence.push(addToSequence)
-
-      this.setState({
-        computerSequence: newSequence
-      })
-      console.log(newSequence);
-      this.animateSequence(newSequence)
-
-    }
-    animateSequence(newSequence) {
-      console.log("It comes into animate sequence");
-      var speedUp = this.state.speed - (this.state.round * 0.2)
-      var that = this
-      for (let i = 0; i < newSequence.length; i++) {
-
-        setTimeout(function() {
-
-          setTimeout(function() {
-            that.setState({
-              lit: newSequence[i]
-            })
-          }, 800)
-
-          if (newSequence[i] === newSequence[i - 1]) {
-            console.log("double up");
-            setTimeout(function() {
-
-              that.setState({
-                lit: 0
-              })
-
-            }, (990))
-
-          } else {
-            setTimeout(function() {
-              that.setState({
-                lit: 0
-              })
-
-            }, (1000))
+          }else{
+            this.onSendScore();
           }
-
-
-        }, (speedUp * i * 1000));
+        })
 
 
       }
+   }
+
+
+
+  }
+
+  addToRound(){
+
+    let currentRound = this.state.round;
+    currentRound ++;
+
+    this.setState({round: currentRound, playerSequence: []});
+
+
+    this.addToSequence();
+  }
+
+  addToSequence (){
+
+    let addToSequence = Math.floor((Math.random()*4)+1)
+    let newSequence  = this.state.computerSequence
+    newSequence.push(addToSequence)
+
+    this.setState({computerSequence:newSequence})
+    console.log(newSequence);
+    this.animateSequence(newSequence)
+
+  }
+  animateSequence(newSequence){
+
+
+    let speed = (this.state.speed - (this.state.round * 0.19));
+    var that = this;
+    for(let i = 0; i<newSequence.length; i++){
+
+        setTimeout(function(){
+
+          setTimeout(function(){
+            that.setState({lit: newSequence[i]})
+          }, 800)
+
+          if(newSequence[i] === newSequence[i-1]){
+
+            setTimeout(function(){
+
+              that.setState({lit:0})
+
+          },990)
+
+          }else{
+          setTimeout(function(){
+            that.setState({lit: 0})
+
+          },1000)
+        }
+
+
+      }, speed * i * 1000);
+
 
     }
-    turnOver() {
 
-      this.onSendScore("done");
+  }
+  turnOver(){
+    this.onSendScore("done");
 
-    }
-    tieGame() {
-      this.setState({
-        gameSetter: "tie"
-      })
-      this.onSendScore("tieGame");
+  }
+  tieGame(){
+    this.setState({gameSetter:"tie"})
+    this.onSendScore("tieGame");
 
-    }
+  }
 
-    gameStatus(status) {
+  gameStatus(status){
 
-      if (status === "winner") {
+      if(status === "winner"){
         return this.gameMessage(" Win");
 
 
-      } else if (status === "tie") {
+      }else if (status === "tie"){
         return this.gameMessage(" tied");
 
 
-      } else {
+      }else{
 
-        return this.gameMessage(" lose");
+          return this.gameMessage(" lose");
 
       }
-    }
+  }
 
-    gameMessage(status) {
-      return <Text > You {
-        status
-      } < /Text>
+  gameMessage(status){
+    return <Text style={styles.score}> You {status} </Text>
 
 
-    }
+  }
 
   }
 
